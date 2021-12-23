e0fd56f4699a1cf6de862f38475a4d22748b30cb
EC2 Default User <ec2-user@ip-172-31-39-36.ec2.internal>
2021-12-23 12:30:18 +0000

    first commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,27 @@
+class BlogsController < ApplicationController
+  
+  def show
+  end
+
+  def index
+  end
+
+  def new
+    @blog = Blog.new
+  end
+  
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
+  end
+
+  def edit
+  end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+  
+end
